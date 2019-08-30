32fc5dd7002dfb600f399ef541b1cca0e36bb24d
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-29 21:53:05 -0400

    Add program and author info at start of game

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -10,6 +10,18 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Angelo Bovino";
+            string authorHomepage = "https://angelobovino.me";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine($"{appName}: Version {appVersion} by {appAuthor} {authorHomepage}");
+
+            Console.ResetColor();
+
+            Console.ReadLine();
         }
     }
 }
